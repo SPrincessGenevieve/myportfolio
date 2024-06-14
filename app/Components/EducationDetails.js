593f71c6d78255abbc7369ce24b0a4ffb10f67de
sprincessgenevieve <sprincessgenevieve@gmail.com>
2024-06-14 14:04:26 +0800

    Added About Section Introduction and Education Details

---
+++ app/Components/EducationDetails.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { motion, Variants } from "framer-motion";
+import './educationDetails.css'
+
+export default function EducationDetails({
+  level,
+  year,
+  course,
+  school,
+  campus,
+  initial,
+  whileInView,
+  transition,
+}) {
+  return (
+    <div className="year-cont">
+      <h1
+      
+        className="tertiary"
+      >
+        {level}
+      </h1>
+      <div
+       
+        className="details"
+      >
+        <p className="details-2">{year}</p>
+        <p className="details-2">{course}</p>
+        <p className="details-2">{school}</p>
+        <p className="details-2">{campus}</p>
+      </div>
+    </div>
+  );
+}
---
+++ app/Components/Overview.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./overview.css";
+import WebDev from "./../assets/css.svg";
+import ReactDev from "./../assets/react.svg";
+import GraphicDev from "./../assets/graphic.svg";
+import Image from "next/image";
+
+export default function Overview() {
+  return (
+    <div className="overview-cont">
+      <div className="overview-l1">
+        <h2 className="intro-over">INTRODUCTION</h2>
+        <h1 className="over">OVERVIEW</h1>
+      </div>
+      <div className="overview-l2">
+        <p className="long-intro">
+          As a final-year college student with a robust foundation in React,
+          HTML5, CSS, and JavaScript, along with familiarity with Jira and REST,
+          I am enthusiastic about contributing to a dynamic team in a role that
+          values innovation and high-quality deliverables. My skills extend
+          beyond technical proficiency; I excel in collaborating with teammates
+          to develop and deliver successful projects.
+        </p>
+      </div>
+      <div className="over-box">
+        <div className="box 1">
+          <Image className="dev css" src={WebDev} alt=""></Image>
+          <h2 className="over-title">WEB DEVELOPER</h2>
+        </div>
+
+        <div className="box 2">
+          <Image className="dev react" src={ReactDev} alt=""></Image>
+          <h2 className="over-title">REACT DEVELOPER</h2>
+        </div>
+
+        <div className="box 3">
+          <Image className="dev graph" src={GraphicDev} alt=""></Image>
+          <h2 className="over-title">GRAPHIC DESIGNER</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
