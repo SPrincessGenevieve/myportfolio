82e867d17fb14d36a81212ec07f4dabd2e817c27
Princess Genevieve Sagrado <sprincessgenevieve@gmail.com>
2024-10-16 23:15:50 +0800

    Updated Projects

--- app/Components/Pot.js
+++ app/Components/Pot.js
@@ -6,53 +6,62 @@ import {
   WebStoriesRounded,
 } from "@mui/icons-material";
 import Link from "next/link";
-import react from './../Assets/react-w.png'
-import next from './../Assets/Next.png'
-import figma from './../Assets/Figma-logo-w.svg.png'
+import react from "./../Assets/react-w.png";
+import next from "./../Assets/Next.png";
+import figma from "./../Assets/Figma-logo-w.svg.png";
+import mindee from "./../Assets/mindee.png";
 import Image from "next/image";
 
-export default function Pot({ gitLink, webLink, desc, title, date, classNameV2 }) {
+export default function Pot({
+  gitLink,
+  webLink,
+  desc,
+  title,
+  date,
+  classNameV2,
+  mainCont,
+  classNameNext,
+  classNameMindee,
+  circleLink,
+}) {
   return (
-    <div className="project-detail-cont">
-      <div className="body-cont-pot">
-        <div className="lid-pot">
-          <div className="lid-pot-2">
-            <div className="circle-cont">
-              <div className="circle-section c-one"></div>
-              <div className="circle-section c-two"></div>
-
-              <Link className="circle-section c-three" href="">
-                <div
-                  className={`circle-section c-three ${classNameV2}`}
-                ></div>
-              </Link>
+    <div className={`project-detail-cont ${mainCont}`}>
+      <div className="main-lid-cont">
+        <div className="lid-top-cont">
+          <div className="cicle-cont">
+            <div className="c-one">
+              <div className="c-two">
+                <Link href={circleLink} className="c-three"><div className={`c-three ${classNameV2}`}></div></Link>
+              </div>
             </div>
-            <div className="lid"></div>
           </div>
         </div>
+      </div>
 
-        <div className="web-title-cont">
-          <p className="pot-name">{title}</p>
-        </div>
-        <div className="lid-body">
-        <div className="lan-cont">
+      <div className="title-cont-pot">
+        <p className="pot-name">{title}</p>
+      </div>
+      <div className="lid-body-cont">
+        <div className="top-pot">
+          <div className="lan-cont">
             <Image className="react-icon" src={react}></Image>
-            <Image className="next-icon" src={next}></Image>
+            <Image className={`next-icon ${classNameNext}`} src={next}></Image>
+            <Image
+              className={`mindee-icon ${classNameMindee}`}
+              src={mindee}
+            ></Image>
           </div>
           <p className="pot-date">{date}</p>
-          
           <p className="pot-details">{desc}</p>
-          
-          <div className="icon-cont-proj">
+        </div>
+        <div className="icon-cont-proj">
             <Link href={gitLink} target="_blank">
               <GitHub className="github-icon" />
             </Link>
             <Link href={webLink}>
               <WebStoriesRounded className="eye-icon" />
             </Link>
-            <Link href=""><Image className="figma-cont"  src={figma}></Image></Link>
           </div>
-        </div>
       </div>
     </div>
   );
--- app/Pages/Project.js
+++ app/Pages/Project.js
@@ -23,27 +23,35 @@ export default function Project() {
           date="JANUARY 2023 – PRESENT"
           desc="Visit my portfolio website to explore a curated collection of my work, showcasing a blend of web development projects and captivating graphic design pieces. Get a glimpse of my skills, creativity, and dedication to delivering impactful digital solutions."
           classNameV2="port-bg"
-        />
+          mainCont="first-cont-item"
+          circleLink=""
+          classNameMindee="visibilityMindee"
+          />
         </div>
         <div className="p-pot">
-          <Pot
+        <Pot
           gitLink=""
           webLink=""
-          title="Portfolio"
-          date="JANUARY 2023 – PRESENT"
-          desc="Visit my portfolio website to explore a curated collection of my work, showcasing a blend of web development projects and captivating graphic design pieces. Get a glimpse of my skills, creativity, and dedication to delivering impactful digital solutions."
-          classNameV2="port-bg"
-        />
+          title="E-TCMF Website"
+          date="DECEMBER 2023 – FEBRUARY 2024"
+          desc="A web-based platform has been developed to empower traffic enforcers, enabling them to efficiently monitor traffic violation citations and generate comprehensive reports for improved traffic management and enforcement."
+          classNameV2="web-bg"
+          circleLink=""
+          classNameNext="visibilityNext"
+          classNameMindee="visibilityMindee"
+          />
         </div>
         <div className="p-pot">
-          <Pot
+        <Pot
           gitLink=""
           webLink=""
-          title="Portfolio"
-          date="JANUARY 2023 – PRESENT"
-          desc="Visit my portfolio website to explore a curated collection of my work, showcasing a blend of web development projects and captivating graphic design pieces. Get a glimpse of my skills, creativity, and dedication to delivering impactful digital solutions."
-          classNameV2="port-bg"
-        />
+          title="E-TCMF Mobile"
+          date="DECEMBER 2023 – FEBRUARY 2024"
+          desc="The system allows users to generate citations via manual input or Mindee's OCR technology, enhancing accuracy and efficiency. It supports real-time violation monitoring and enables instant data transfers, streamlining the citation process."
+          classNameV2="mob-bg"
+          circleLink=""
+          classNameNext="visibilityNext"
+          />
         </div>
      
       </div>
